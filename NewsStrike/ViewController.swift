35c011d20f903cd93b164c8e2048d8abc2f9e095
Shresth Pratap Singh <shresthsingh3310@gmail.com>
2019-06-08 23:14:46 +0530

    Initial Commit

---
+++ NewsStrike/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsStrike
+//
+//  Created by Shresth Pratap Singh on 08/06/19.
+//  Copyright © 2019 Shresth Pratap Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
